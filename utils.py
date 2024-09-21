e68eb695e57d7ba311bb3a1ffba266c7ab4b3cd9
z3lx <57017122+z3lx@users.noreply.github.com>
2024-09-21 13:43:42 -0400

    Remove debug print in utils

--- utils.py
+++ utils.py
@@ -5,6 +5,5 @@ def try_cast(value: Any, cast: Type) -> Any:
     try:
         return cast(value)
     except ValueError:
-        print("a")
         print(f"Veuillez entrer un {cast.__name__}")
         return None
