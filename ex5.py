c1f23615e5d685e846ca1f9351880d58041e1b65
z3lx <57017122+z3lx@users.noreply.github.com>
2024-09-21 13:06:30 -0400

    Implement ex5

--- ex5.py
+++ ex5.py
@@ -1,5 +1,16 @@
 #TODO: Analyser la chaîne de caractères saisie et compter le nombre de médailles.
 #      Attention si la chaîne est invalide, un message d'erreur est attendu.
+import re
 
-country = ...
-code_medals = ...
+country = input("Pays concerné ? ")
+code_medals = input("Chaine représentant les médailles ? ")
+
+valid_medal_codes = "GSB"
+pattern = re.compile(f"[{valid_medal_codes}]*")
+if not re.fullmatch(pattern, code_medals):
+    print("Ceci est une chaine invalide.")
+else:
+    g = code_medals.count("G")
+    s = code_medals.count("S")
+    b = code_medals.count("B")
+    print(f"{country}:\n- {g} Or\n- {s} Argent\n- {b} Bronze")
