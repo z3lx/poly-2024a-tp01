3261188143356667bf506146260e99b0f78d80ef
z3lx <57017122+z3lx@users.noreply.github.com>
2024-09-21 12:44:47 -0400

    Implement ex4

--- ex4.py
+++ ex4.py
@@ -3,4 +3,23 @@
 #        et affiche le résultat au format "XX km".
 #        Assurez une gestion du pourcentage valide au cours de votre programme (% toujours dans [0 ; 100]).
 
-battery_level = ...
+battery_level = float(input("Pourcentage de batterie ? "))
+
+if battery_level <= 0:
+    print("La batterie est vide")
+else:
+    battery_multiplier_map = {
+        50: 2,
+        25: 0.5,
+        10: 1,
+        5: 2.5,
+        0: 6
+    }
+
+    distance = 0
+    for percentage, multiplier in battery_multiplier_map.items():
+        if battery_level > percentage:
+            distance += multiplier * (battery_level - percentage)
+            battery_level = percentage
+
+    print(f"{distance} km")
